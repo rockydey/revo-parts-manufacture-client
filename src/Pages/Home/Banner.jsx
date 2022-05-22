298c1b6d45bb27c7559ced28f3f244dae6cd6580
rockydey <coderrocky25@gmail.com>
2022-05-22 20:23:38 +0600

    home page banner section complete

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import banner from '../../assists/banner.png';
+
+const Banner = () => {
+    return (
+        <div className='h-[60vh] flex flex-col items-center justify-center' style={{ background: `url(${banner})`, backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+            <h1 className='text-slate-100 md:text-5xl text-2xl tracking-wider'>YOUR CAR PARTS SUPPLIER</h1>
+            <h3 className='text-slate-100 md:text-3xl text-lg md:mt-5 mt-4 lg:w-3/5 w-full text-center tracking-wider'>FOR ASIAN CARS, ELECTRIC CARS AND SHOCK ABSORBERS</h3>
+            <a href='#parts' className='btn btn-primary md:mt-5 mt-4 text-base font-semibold'>Explore</a>
+        </div>
+    );
+};
+
+export default Banner;
