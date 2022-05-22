0af279dd833980fec0cea115ec05a272a085d264
rockydey <coderrocky25@gmail.com>
2022-05-22 15:28:48 +0600

    install react app, react route, tailwind, and daisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#cc2b5e",
+          secondary: "#753a88",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")]
+}
