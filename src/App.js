34b9e970850baf504a98ab97e96d2534fb26d802
rockydey <coderrocky25@gmail.com>
2022-05-22 15:42:57 +0600

    all routes setup complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Pages/Authentication/Login';
+import SignUp from './Pages/Authentication/SignUp';
+import Blogs from './Pages/Blogs/Blogs';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Home from './Pages/Home/Home';
+import MyPortfolio from './Pages/MyPortfolio/MyPortfolio';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/myPortfolio' element={<MyPortfolio />}></Route>
+        <Route path='/signUp' element={<SignUp />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
